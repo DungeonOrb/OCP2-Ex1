2fea49cb568bf2e18eaf509fa15acc219fb84ad3
Ugo Leone <ugomael@gmail.com>
2025-10-31 17:29:07 +0100

    ajout de la commande list

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <?php
-while (true) {
+
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
-}
+    if($line = "list") {
+        echo "affichage de la liste";
+    }
