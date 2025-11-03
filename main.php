4a42d11ef1a647a07349e513de1d9f5123d535fd
Ugo Leone <ugomael@gmail.com>
2025-11-03 11:57:47 +0100

    ajout de la fonction findAll() dans la commande list

--- main.php
+++ main.php
@@ -1,12 +1,8 @@
 <?php
 require 'DBConnect.php';
 require 'ContactManager.php';
-//$contacts = $db->query('SELECT * FROM contact ORDER BY id ASC');
-//    $line = readline("Entrez votre commande : ");
-//    if($line = "list") {
-//        echo "affichage de la liste";
-//        foreach($contacts as $contact){
-//            echo $contact['id'];
-//        }
-//    }
-ContactManager::findAll();
+    $line = readline("Entrez votre commande : ");
+    if($line = "list") {
+        echo "affichage de la liste \n";
+            ContactManager::findAll();
+        }
